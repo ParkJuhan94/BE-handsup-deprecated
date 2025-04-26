3d7b91ae308c3cb4de9a379d1e1e317918a721d4
박주한 <1004sunye@naver.com>
2025-04-27 03:41:06 +0900

    [chore] : CORS 정책 수정 - 프론트엔드 요청 허용 (#25) (#28)
    * [chore] : CD에서 EC2 접속 방식을 SSH 키 방식으로 변경
    
    * [chore] : swagger server 로컬만 허용
    
    * [chore] : build.gradle 개선
    
    * [chore] : kafka 컨테이너 분리
    
    * [chore] : CORS 정책 수정 - 프론트엔드 요청 허용 (#25)

--- api/src/main/java/dev/handsup/common/config/WebConfig.java
+++ api/src/main/java/dev/handsup/common/config/WebConfig.java
@@ -2,30 +2,44 @@ package dev.handsup.common.config;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import dev.handsup.auth.jwt.JwtAuthorizationArgumentResolver;
 import dev.handsup.auth.jwt.JwtInterceptor;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final JwtAuthorizationArgumentResolver jwtAuthorizationArgumentResolver;
-	private final JwtInterceptor jwtInterceptor;
+    private final JwtAuthorizationArgumentResolver jwtAuthorizationArgumentResolver;
+    private final JwtInterceptor jwtInterceptor;
 
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-		argumentResolvers.add(jwtAuthorizationArgumentResolver);
-	}
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(jwtAuthorizationArgumentResolver);
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(jwtInterceptor)
-			.addPathPatterns("/api/**");
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+            .addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "https://localhost:8080"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true); // 쿠키/인증 헤더 사용 허용
+    }
 }
