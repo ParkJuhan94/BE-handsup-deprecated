99dc8fd6871ece35242259e3d95c6526ab885441
ParkJuHan94 <1004sunye@naver.com>
2025-04-28 23:25:53 +0900

    [feat] : AuctionService 에서 getRecommendAuctions() 제거하고 AuctionCacheService에 구현 (#23)

--- core/src/main/java/dev/handsup/auction/service/AuctionService.java
+++ core/src/main/java/dev/handsup/auction/service/AuctionService.java
@@ -2,6 +2,8 @@ package dev.handsup.auction.service;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -23,59 +25,51 @@ import dev.handsup.common.dto.CommonMapper;
 import dev.handsup.common.dto.PageResponse;
 import dev.handsup.common.exception.NotFoundException;
 import dev.handsup.user.domain.User;
-import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuctionService {
 
-	private final AuctionRepository auctionRepository;
-	private final ProductCategoryRepository productCategoryRepository;
-	private final PreferredProductCategoryRepository preferredProductCategoryRepository;
-	private final AuctionQueryRepository auctionQueryRepository;
+    private final AuctionRepository auctionRepository;
+    private final ProductCategoryRepository productCategoryRepository;
+    private final PreferredProductCategoryRepository preferredProductCategoryRepository;
+    private final AuctionQueryRepository auctionQueryRepository;
 
-	public AuctionDetailResponse registerAuction(RegisterAuctionRequest request, User user) {
-		ProductCategory productCategory = getProductCategoryByValue(request.productCategory());
-		Auction auction = AuctionMapper.toAuction(request, productCategory, user);
-		return AuctionMapper.toAuctionDetailResponse(auctionRepository.save(auction));
-	}
+    public AuctionDetailResponse registerAuction(RegisterAuctionRequest request, User user) {
+        ProductCategory productCategory = getProductCategoryByValue(request.productCategory());
+        Auction auction = AuctionMapper.toAuction(request, productCategory, user);
+        return AuctionMapper.toAuctionDetailResponse(auctionRepository.save(auction));
+    }
 
-	@Transactional(readOnly = true)
-	public AuctionDetailResponse getAuctionDetail(Long auctionId) {
-		Auction auction = getAuctionById(auctionId);
-		return AuctionMapper.toAuctionDetailResponse(auction);
-	}
+    @Transactional(readOnly = true)
+    public AuctionDetailResponse getAuctionDetail(Long auctionId) {
+        Auction auction = getAuctionById(auctionId);
+        return AuctionMapper.toAuctionDetailResponse(auction);
+    }
 
-	@Transactional(readOnly = true)
-	public PageResponse<RecommendAuctionResponse> getRecommendAuctions(String si, String gu, String dong,
-		Pageable pageable) {
-		Slice<RecommendAuctionResponse> auctionResponsePage = auctionQueryRepository
-			.sortAuctionByCriteria(si, gu, dong, pageable)
-			.map(AuctionMapper::toRecommendAuctionResponse);
-		return CommonMapper.toPageResponse(auctionResponsePage);
-	}
+    @Transactional(readOnly = true)
+    public PageResponse<RecommendAuctionResponse> getUserPreferredCategoryAuctions(User user,
+        Pageable pageable) {
+        List<ProductCategory> productCategories = preferredProductCategoryRepository.findByUser(
+                user)
+            .stream()
+            .map(PreferredProductCategory::getProductCategory)
+            .toList();
 
-	@Transactional(readOnly = true)
-	public PageResponse<RecommendAuctionResponse> getUserPreferredCategoryAuctions(User user, Pageable pageable) {
-		List<ProductCategory> productCategories = preferredProductCategoryRepository.findByUser(user)
-			.stream()
-			.map(PreferredProductCategory::getProductCategory)
-			.toList();
+        Slice<RecommendAuctionResponse> auctionResponsePage = auctionQueryRepository
+            .findByProductCategories(productCategories, pageable)
+            .map(AuctionMapper::toRecommendAuctionResponse);
 
-		Slice<RecommendAuctionResponse> auctionResponsePage = auctionQueryRepository
-			.findByProductCategories(productCategories, pageable)
-			.map(AuctionMapper::toRecommendAuctionResponse);
+        return CommonMapper.toPageResponse(auctionResponsePage);
+    }
 
-		return CommonMapper.toPageResponse(auctionResponsePage);
-	}
+    private ProductCategory getProductCategoryByValue(String productCategoryValue) {
+        return productCategoryRepository.findByValue(productCategoryValue)
+            .orElseThrow(() -> new NotFoundException(AuctionErrorCode.NOT_FOUND_PRODUCT_CATEGORY));
+    }
 
-	private ProductCategory getProductCategoryByValue(String productCategoryValue) {
-		return productCategoryRepository.findByValue(productCategoryValue)
-			.orElseThrow(() -> new NotFoundException(AuctionErrorCode.NOT_FOUND_PRODUCT_CATEGORY));
-	}
-
-	public Auction getAuctionById(Long auctionId) {
-		return auctionRepository.findById(auctionId)
-			.orElseThrow(() -> new NotFoundException(AuctionErrorCode.NOT_FOUND_AUCTION));
-	}
+    public Auction getAuctionById(Long auctionId) {
+        return auctionRepository.findById(auctionId)
+            .orElseThrow(() -> new NotFoundException(AuctionErrorCode.NOT_FOUND_AUCTION));
+    }
 }
