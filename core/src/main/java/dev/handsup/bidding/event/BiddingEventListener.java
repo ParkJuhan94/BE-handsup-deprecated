d0a2ed59eebdd1c099fa488de664c635d9434394
ParkJuHan94 <1004sunye@naver.com>
2025-05-12 13:21:31 +0900

    [style] : BiddingEventListener와 내부 메서드도 네이밍 변경 (#47)

--- core/src/main/java/dev/handsup/bidding/event/BiddingEventListener.java
+++ core/src/main/java/dev/handsup/bidding/event/BiddingEventListener.java
@@ -1,24 +1,24 @@
-package dev.handsup.common.event;
+package dev.handsup.bidding.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import dev.handsup.event.common.BiddingEvent;
-import dev.handsup.event.producer.BiddingEventProducer;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class BiddingEventHandler {
+@ConditionalOnProperty(name = "event.kafka.enabled", havingValue = "true", matchIfMissing = false)
+public class BiddingEventListener {
 
     private final BiddingEventProducer biddingEventProducer;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleRegisterBidding(BiddingEvent event) {
+    public void listen(BiddingEvent event) {
         biddingEventProducer.send(event);
     }
 }
