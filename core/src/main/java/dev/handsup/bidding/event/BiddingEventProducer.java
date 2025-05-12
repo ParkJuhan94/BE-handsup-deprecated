02e40e8cfe457d1aeec764257cb735826f65c1e8
ParkJuHan94 <1004sunye@naver.com>
2025-05-12 13:10:49 +0900

    [refactor] : BIDDING_TOPIC_NAME 상수화 (#47)

--- core/src/main/java/dev/handsup/bidding/event/BiddingEventProducer.java
+++ core/src/main/java/dev/handsup/bidding/event/BiddingEventProducer.java
@@ -1,20 +1,19 @@
-package dev.handsup.event.producer;
+package dev.handsup.bidding.event;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import dev.handsup.event.common.BiddingEvent;
-
 
 @Component
 @RequiredArgsConstructor
 public class BiddingEventProducer {
 
+    private static final String BIDDING_TOPIC_NAME = "bidding-events";
     private final KafkaTemplate<String, BiddingEvent> kafkaTemplate;
 
     public void send(BiddingEvent event) {
-        kafkaTemplate.send("bidding-events", event);
+        kafkaTemplate.send(BIDDING_TOPIC_NAME, event);
     }
 }
