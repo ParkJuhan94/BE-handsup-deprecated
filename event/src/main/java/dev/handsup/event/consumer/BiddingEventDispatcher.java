4e00284160642a1d92d5a54b8662513ec9877109
ParkJuHan94 <1004sunye@naver.com>
2025-05-14 17:58:57 +0900

    [chore] : @KafkaListner에서 retry & dlt 구현 (#56)

--- event/src/main/java/dev/handsup/event/consumer/BiddingEventDispatcher.java
+++ event/src/main/java/dev/handsup/event/consumer/BiddingEventDispatcher.java
@@ -7,10 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import dev.handsup.bidding.event.BiddingEvent;
+import dev.handsup.event.common.EventHandler;
 
 @Slf4j
 @Component
@@ -19,8 +22,21 @@ import dev.handsup.bidding.event.BiddingEvent;
 public class BiddingEventDispatcher {
 
     private static final String BIDDING_TOPIC_NAME = "bidding-events";
+    private static final String BIDDING_TOPIC_DLT_NAME = BIDDING_TOPIC_NAME + ".dlt";
     private final List<EventHandler<BiddingEvent>> handlers;
 
+    @RetryableTopic(
+        attempts = "4", // 최초 1회 + retry 3회
+        backoff = @Backoff(
+            delay = 1000, // 1초 후 첫 재시도
+            multiplier = 2.0, // 이후 2초 → 4초
+            maxDelay = 5000 // 최대 5초까지만 delay
+        ),
+        dltTopicSuffix = ".dlt", // 실패 시 전송될 DLT 토픽 접미사
+        autoCreateTopics = "true",
+        include = {RuntimeException.class}, // 대상 예외 지정 (생략 시 모든 Exception),
+        timeout = "60000" // 전체 재시도 타임아웃 제한 (1분)
+    )
     @KafkaListener(
         topics = BIDDING_TOPIC_NAME,
         groupId = "bidding-consumer-group",
@@ -40,6 +56,15 @@ public class BiddingEventDispatcher {
         if (!handled) {
             log.warn("[BIDDING_EVENT] No matching handler found for event: {}", event);
         }
+    }
 
+    @KafkaListener(
+        topics = BIDDING_TOPIC_DLT_NAME,
+        groupId = "bidding-consumer-group-dlt",
+        containerFactory = "biddingKafkaListenerContainerFactory"
+    )
+    public void handleDLT(@Payload BiddingEvent event) {
+        log.error("❌ DLT 메시지 도착: {}", event);
+        // TODO: Elastic에 기록 or DB 저장 등 처리
     }
 }
