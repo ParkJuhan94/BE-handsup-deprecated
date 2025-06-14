2e008e59771a420a7f3a393e5c935d5d86661d62
ParkJuHan94 <1004sunye@naver.com>
2025-06-14 20:29:09 +0900

    style: core 모듈의 kafka 디렉토리로 이동 (#79)

--- core/src/main/java/dev/handsup/common/repository/ProcessedEventLogRepository.java
+++ core/src/main/java/dev/handsup/common/repository/ProcessedEventLogRepository.java
@@ -3,7 +3,7 @@ package dev.handsup.common.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import dev.handsup.common.entity.ProcessedEvent;
+import dev.handsup.kafka.domain.ProcessedEvent;
 
 @Repository
 public interface ProcessedEventLogRepository extends JpaRepository<ProcessedEvent, Long> {
