85c7cd0cfca40ca199c30ed5bc5569b1d332243b
ParkJuHan94 <1004sunye@naver.com>
2025-07-27 07:44:02 +0900

    fix: 삭제했던 CoreTestApplication 복구 (#90)

--- core/src/test/java/dev/handsup/auction/repository/auction/AuctionRepositoryTest.java
+++ core/src/test/java/dev/handsup/auction/repository/auction/AuctionRepositoryTest.java
@@ -28,53 +28,53 @@ import dev.handsup.user.repository.UserRepository;
 @DisplayName("[AuctionRepository 테스트]")
 class AuctionRepositoryTest extends DataJpaTestSupport {
 
-	private final String DIGITAL_DEVICE = "디지털 기기";
-	private final PageRequest pageRequest = PageRequest.of(0, 10);
+    private static final String DIGITAL_DEVICE = "디지털 기기";
+    private final PageRequest pageRequest = PageRequest.of(0, 10);
 
-	@Autowired
-	private AuctionRepository auctionRepository;
+    @Autowired
+    private AuctionRepository auctionRepository;
 
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private ProductCategoryRepository productCategoryRepository;
-	@Autowired
-	private BookmarkRepository bookmarkRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
 
-	private User user;
-	private ProductCategory category;
+    private User user;
+    private ProductCategory category;
 
-	@BeforeEach
-	void setUp() {
-		category = ProductFixture.productCategory(DIGITAL_DEVICE);
-		productCategoryRepository.save(category);
+    @BeforeEach
+    void setUp() {
+        category = ProductFixture.productCategory(DIGITAL_DEVICE);
+        productCategoryRepository.save(category);
 
-		user = UserFixture.user1();
-		userRepository.save(user);
-	}
+        user = UserFixture.user1();
+        userRepository.save(user);
+    }
 
-	@DisplayName("[유저가 북마크한 경매를 모두 조회할 수 있다.]")
-	@Test
-	void findBookmarkAuction() {
-		//given
-		Auction auction1 = AuctionFixture.auction(category);
-		Auction auction2 = AuctionFixture.auction(category);
-		auctionRepository.saveAll(List.of(auction1, auction2));
+    @DisplayName("[유저가 북마크한 경매를 모두 조회할 수 있다.]")
+    @Test
+    void findBookmarkAuction() {
+        //given
+        Auction auction1 = AuctionFixture.auction(category);
+        Auction auction2 = AuctionFixture.auction(category);
+        auctionRepository.saveAll(List.of(auction1, auction2));
 
-		Bookmark bookmark1 = BookmarkFixture.bookmark(user, auction1);
-		Bookmark bookmark2 = BookmarkFixture.bookmark(user, auction2);
-		bookmarkRepository.saveAll(List.of(bookmark1, bookmark2));
+        Bookmark bookmark1 = BookmarkFixture.bookmark(user, auction1);
+        Bookmark bookmark2 = BookmarkFixture.bookmark(user, auction2);
+        bookmarkRepository.saveAll(List.of(bookmark1, bookmark2));
 
-		//when
-		Slice<Auction> auctionSlice = auctionRepository.findBookmarkAuction(user, pageRequest);
-		List<Auction> auctions = auctionSlice.getContent();
-		//then
-		assertAll(
-			() -> assertThat(auctionSlice).hasSize(2),
-			() -> assertThat(auctionSlice.hasNext()).isFalse(),
-			() -> assertThat(auctions.get(0)).isEqualTo(auction1),
-			() -> assertThat(auctions.get(1)).isEqualTo(auction2)
-		);
+        //when
+        Slice<Auction> auctionSlice = auctionRepository.findBookmarkAuction(user, pageRequest);
+        List<Auction> auctions = auctionSlice.getContent();
+        //then
+        assertAll(
+            () -> assertThat(auctionSlice).hasSize(2),
+            () -> assertThat(auctionSlice.hasNext()).isFalse(),
+            () -> assertThat(auctions.get(0)).isEqualTo(auction1),
+            () -> assertThat(auctions.get(1)).isEqualTo(auction2)
+        );
 
-	}
+    }
 }
