5b1bb8b11ed59e3f017d432cdba8631633954ac8
ParkJuHan94 <1004sunye@naver.com>
2025-08-05 00:00:33 +0900

    feat: Elasticsearch 의존성, Config 추가 (#84)

---
+++ core/src/main/java/dev/handsup/common/config/ElasticsearchConfig.java
@@ -0,0 +1,32 @@
+package dev.handsup.common.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder(
+            new HttpHost("localhost", 9200, "http") // 운영이면 ES 호스트 변경
+        ).build();
+    }
+
+    @Bean
+    public ElasticsearchTransport transport(RestClient restClient) {
+        return new RestClientTransport(restClient, new JacksonJsonpMapper());
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+        return new ElasticsearchClient(transport);
+    }
+}
